0c2c9999f57b3e345406443d44279e5bd880dff0
Ridwan <ridwan.pust@gmail.com>
2021-05-15 07:26:53 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OopWithCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
